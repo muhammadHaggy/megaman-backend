a3092bb9d1a91c34bddddc369c720ca2979ee5f3
Muhammad Haggy <muhammad.haggy@ui.ac.id>
2024-05-25 14:06:57 +0700

    linter

--- src/trackers/trackers.controller.ts
+++ src/trackers/trackers.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { TrackersService } from './trackers.service';
 import { CreateTrackerDto } from './dto/create-tracker.dto';
 import { UpdateTrackerDto } from './dto/update-tracker.dto';
