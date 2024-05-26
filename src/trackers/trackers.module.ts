d1b23aa65eb5256f95e136dc85f1ec538e248f19
muhammad_haggy <muhammad.haggy@ui.ac.id>
2024-05-26 11:24:46 +0700

    fix bug

--- src/trackers/trackers.module.ts
+++ src/trackers/trackers.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TrackersService } from './trackers.service';
 import { TrackersController } from './trackers.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [TrackersController],
-  providers: [TrackersService],
+  providers: [TrackersService, PrismaService]
 })
 export class TrackersModule {}
