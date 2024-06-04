bf5bd39837d7a48ef57ad3c6f47c371ed0fce9dc
Muhammad Haggy <muhammad.haggy@ui.ac.id>
2024-06-04 12:58:01 +0700

    add image endpoint for asset

--- src/assets/assets.controller.ts
+++ src/assets/assets.controller.ts
@@ -6,18 +6,64 @@ import {
   Patch,
   Param,
   Delete,
+  HttpException,
+  HttpStatus,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { AssetsService } from './assets.service';
 import { CreateAssetDto } from './dto/create-asset.dto';
 import { UpdateAssetDto } from './dto/update-asset.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { User } from 'src/common/decorators/user.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { extname } from 'path';
 
 @Controller('assets')
 @ApiTags('Assets')
 @ApiBearerAuth()
 export class AssetsController {
   constructor(private readonly assetsService: AssetsService) {}
+
+  @Post(':id/image')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      limits: { fileSize: 1024 * 1024 },
+      fileFilter: (req: any, file: any, cb: any) => {
+        if (file.mimetype.match(/\/(jpg|jpeg|png|gif|webp)$/)) {
+          // Allow storage of file
+          cb(null, true);
+        } else {
+          // Reject file
+          cb(
+            new HttpException(
+              `Unsupported file type ${extname(file.originalname)}`,
+              HttpStatus.BAD_REQUEST
+            ),
+            false
+          );
+        }
+      },
+    })
+  )
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  uploadImage(
+    @UploadedFile() file: Express.Multer.File,
+    @Param('id') assetId: number
+  ) {
+    return this.assetsService.uploadImage(assetId, file);
+  }
 
   @Post()
   create(
--- src/assets/assets.service.ts
+++ src/assets/assets.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateAssetDto } from './dto/create-asset.dto';
 import { UpdateAssetDto } from './dto/update-asset.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as sharp from 'sharp';
 
 @Injectable()
 export class AssetsService {
@@ -35,6 +36,20 @@ export class AssetsService {
   remove(id: number) {
     return this.prismaService.asset.delete({
       where: { id },
+    });
+  }
+
+  async uploadImage(assetId: number, file: Express.Multer.File) {
+    const buffer = await sharp(file.buffer)
+      .resize(150, 150)
+      .jpeg({ quality: 75 })
+      .toBuffer();
+
+    return this.prismaService.asset.update({
+      where: { id: assetId },
+      data: {
+        image: buffer,
+      },
     });
   }
 }
