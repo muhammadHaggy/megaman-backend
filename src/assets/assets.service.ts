601f1a5df1a73b625a4ae145760657d244ce7b9d
vinsen_wijaya <36811691+vwjaya32@users.noreply.github.com>
2024-06-07 07:51:22 +0700

    feat: upload image to Cloud Storage

--- src/assets/assets.service.ts
+++ src/assets/assets.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { CreateAssetDto } from './dto/create-asset.dto';
 import { UpdateAssetDto } from './dto/update-asset.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Storage } from '@google-cloud/storage';
 import * as sharp from 'sharp';
 
 @Injectable()
 export class AssetsService {
   constructor(private prismaService: PrismaService) {}
+  private storage = new Storage();
+  private bucket = this.storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
+
   create(createAssetDto: CreateAssetDto, userId: number) {
     return this.prismaService.asset.create({
       data: {
@@ -40,17 +44,41 @@ export class AssetsService {
   }
 
   async uploadImage(assetId: number, file: Express.Multer.File) {
-    const buffer = await sharp(file.buffer)
-      .resize(150, 150)
-      .jpeg({ quality: 75 })
-      .toBuffer();
+    console.log(file);
+    const destination = String(assetId);
+    const contentType = file.mimetype;
 
-    return this.prismaService.asset.update({
-      where: { id: assetId },
-      data: {
-        image: buffer,
-      },
-    });
+    try {
+      // Process the image using sharp
+      const buffer = await sharp(file.buffer)
+        .resize(150, 150)
+        .jpeg({ quality: 75 })
+        .toBuffer();
+
+      // Create a blob in the bucket and upload the buffer
+      const blob = this.bucket.file(destination);
+      const blobStream = blob.createWriteStream({
+        resumable: false,
+        metadata: {
+          contentType,
+        },
+      });
+
+      return new Promise((resolve, reject) => {
+        blobStream.on('finish', () => {
+          const publicUrl = `https://storage.googleapis.com/${process.env.GCLOUD_STORAGE_BUCKET}/${assetId}`;
+          resolve({ url: publicUrl });
+        });
+
+        blobStream.on('error', (err) => {
+          console.error('Error uploading file to Google Cloud Storage:', err);
+        });
+
+        blobStream.end(buffer);
+      });
+    } catch (err) {
+      console.error('Error processing file:', err);
+    }
   }
 
   async getAssetsWithoutTracker() {
